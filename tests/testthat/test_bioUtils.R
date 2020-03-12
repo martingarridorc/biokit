c7f3a6ce52b7c86a813552120443800a860e0154
Martin Garrido-Rodriguez <mgrcprof@gmail.com>
2020-03-12 20:00:48 +0100

    increase test coverage

--- tests/testthat/test_bioUtils.R
+++ tests/testthat/test_bioUtils.R
@@ -1,21 +1,32 @@
+library(org.Hs.eg.db)
 # prepare data for testing
+data("cbdData")
+cbdMat <- cbdMat[1:100,]
 intMat <- matrix(1:60, ncol = 6, nrow = 10)
 rownames(intMat) <- letters[1:10]
 testDf <- data.frame(from = rownames(intMat), to = rep(c(NA, "A", "B", "C", "D", "E"), c(2, 2, 1, 3, 1, 1)), stringsAsFactors = FALSE)
 
 test_that("Message outputs nice", {
-    
+
     m <- evaluate_promise(biokit:::messageMappingInfo(testDf, "from", "to"))$messages
     expect_equal(substr(m, 245, 290), "Input keys were finally mapped to 5 target ids")
-    
+
 })
 
 test_that("Summarise matrix", {
-    
+
     res <- summariseMatrix(x = intMat, df = testDf, sourceKey = "from", targetKey = "to", summariseFun = sum)
     expect_type(res, "integer")
     expect_equal(class(res), "matrix")
     expect_true(all(rownames(res) %in% testDf$to))
     expect_equal(as.numeric(rowSums(res)[3]), 576)
-    
+
 })
+
+test_that("Summarise matrix", {
+
+    t <- translateMatrix(x = cbdMat, db = org.Hs.eg.db, sourceKey = "ENTREZID", targetKey = "SYMBOL", summariseFun = sum)
+    expect_equal(class(t), "matrix")
+
+})
+
