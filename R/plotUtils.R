359f0c9ef75604d54696cd2fafd51f8c330f6f10
Martin Garrido-Rodriguez <mgrcprof@gmail.com>
2020-03-13 14:39:48 +0100

    remove ggalt depend and feature to avoid build error (proj4) issue

--- R/plotUtils.R
+++ R/plotUtils.R
@@ -20,20 +20,20 @@
 #'
 #' @import ggplot2
 #'
-defaultVolcano <- function(x, fcCol = "logFc", pCol = "pAdj", statusCol = "status", compCol = NULL, featCol = "feature", upColor = "red", noChangeColor = "black", downColor = "blue", 
+defaultVolcano <- function(x, fcCol = "logFc", pCol = "pAdj", statusCol = "status", compCol = NULL, featCol = "feature", upColor = "red", noChangeColor = "black", downColor = "blue",
     upLabel = "Up", noChangeLabel = "No change", downLabel = "Down") {
-    
+
     # prepare color values
     colVal <- c(upColor, noChangeColor, downColor)
     names(colVal) <- c(upLabel, noChangeLabel, downLabel)
     # tidy eval for column names (https://tidyeval.tidyverse.org/introduction.html)
-    p <- ggplot(x, aes(x = !!sym(fcCol), y = -log10(!!sym(pCol)), color = !!sym(statusCol), feature = !!sym(featCol))) + geom_point(alpha = 0.5) + scale_color_manual(values = colVal) + 
+    p <- ggplot(x, aes(x = !!sym(fcCol), y = -log10(!!sym(pCol)), color = !!sym(statusCol), feature = !!sym(featCol))) + geom_point(alpha = 0.5) + scale_color_manual(values = colVal) +
         theme_bw()
     # if comparison column specified, facet wrap using it
-    if (!is.null(compCol)) 
+    if (!is.null(compCol))
         p <- p + facet_wrap(facets = vars(!!sym(compCol)))
     return(p)
-    
+
 }
 
 
@@ -46,31 +46,27 @@ defaultVolcano <- function(x, fcCol = "logFc", pCol = "pAdj", statusCol = "statu
 #' @param sampInfoDf Data frame containing sample information. Rownames should match \link[stats]{prcomp} matrix rownames.
 #' @param groupCol Column of the data frame that indicates group of samples.
 #' @param showLabel Show row labels?
-#' @param encircle Encircle points by group?
 #'
 #' @return A ggplot containing the default pca plot.
 #'
 #' @export
 #'
 #' @import ggplot2
-#' @importFrom ggalt geom_encircle
 #' @importFrom tibble rownames_to_column
 #'
-defaultPcaPlot <- function(x, sampInfoDf, groupCol, showLabel = TRUE, encircle = TRUE) {
-    
+defaultPcaPlot <- function(x, sampInfoDf, groupCol, showLabel = TRUE) {
+
     # check sampInfo rownames
-    if (!all(rownames(sampInfoDf) == rownames(x$result$x))) 
+    if (!all(rownames(sampInfoDf) == rownames(x$result$x)))
         stop("Rownames of the sample information data frame could not be mapped to pca result.")
     # create df to plot
     toPlotDf <- cbind(tibble::rownames_to_column(sampInfoDf, var = "id"), x$result$x)
     # create plot
-    p <- ggplot(toPlotDf, mapping = aes(x = !!sym("PC1"), y = !!sym("PC2"), color = !!sym(groupCol), fill = !!sym(groupCol), label = !!sym("id"))) + geom_point() + xlab(x$pcts[1]) + 
+    p <- ggplot(toPlotDf, mapping = aes(x = !!sym("PC1"), y = !!sym("PC2"), color = !!sym(groupCol), fill = !!sym(groupCol), label = !!sym("id"))) + geom_point() + xlab(x$pcts[1]) +
         ylab(x$pcts[2]) + theme_bw()
-    # add label and encircle if required
-    if (encircle) 
-        p <- p + ggalt::geom_encircle(expand = 0, alpha = 0.3)
-    if (showLabel) 
+    # add label if required
+    if (showLabel)
         p <- p + geom_label(fill = "white")
     return(p)
-    
+
 }
